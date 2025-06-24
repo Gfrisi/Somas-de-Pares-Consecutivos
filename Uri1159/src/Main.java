1a8f84a02e3555ac0d1179a27240a8637fcfbe0e
Gfrisi <166542513+Gfrisi@users.noreply.github.com>
2025-06-24 11:35:26 -0300

    Update Main.java

--- Uri1159/src/Main.java
+++ Uri1159/src/Main.java
@@ -9,12 +9,12 @@ public class Main {
 		
 		while (x != 0) {
 			
-			// se for impar, some mais 1
+			
 			if (x % 2 != 0) {
 				x = x + 1;
 			}
 			
-			// expressao simplificada: 5 * x + 20
+			
 			int soma = x + x + 2 + x + 4 + x + 6 + x + 8; 
 			System.out.println(soma);
 			
